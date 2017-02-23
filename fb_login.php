8e55404a17fdfc29b2961398d89dfd5ebea7a527
Dannie <dannienctu@gmail.com>
2017-02-23 14:27:50 +0800

    fb test

--- fb_login.php
+++ fb_login.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <script type="text/javascript" src="./fbapp/fb.js"></script>
-<div class="fb-login-button" data-scope="public_profile,email"></div>
+<div class="fb-login-button" data-scope="public_profile,email" onlogin="checkLoginState();"></div>
 <?php
 	session_start();
 	if(isset($_SESSION['name']))
