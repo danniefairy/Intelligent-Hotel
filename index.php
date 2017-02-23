16fc7a895b5f2ce9e7ab3b094e8643b8b8d87cab
Dannie <dannienctu@gmail.com>
2017-02-23 19:03:14 +0800

    fb test

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
       <div class="material-button alt-2"><span class="shape"></span></div>
 
       <div class="title"  style="text-align:center;">
-        <h2 style="font-size:2vw;">Are you ready for your journey?</h2>
+        <h2 style="font-size:2.5vw;">Ready for your journey</h2>
         <h2 style="font-size:2vw;">Let's login and fulfill your dream!</h2>
         <!--FB登入-->
         <script type="text/javascript" src="./fbapp/fb.js"></script>
