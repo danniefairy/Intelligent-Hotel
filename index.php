570caac55ad202230bdd6e0758391ae139b65573
Dannie <dannienctu@gmail.com>
2017-02-23 20:37:18 +0800

    fb test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
         <div class="title">Intelligent Hotel Service</div>
             <h2>&nbsp</h2><br>
-            <img style="float:right; height:200px;" src="./login/icon.jpg" >
+            <img style="float:right; height:300px;" src="./login/icon.jpg" >
             <h2 style="font-size:2vw;">I.H.S provide every traveler the most convinent and diversified vacation. Without any annoying preparation for your itinerary, I.H.S can help you schedule your tour and give you a unforgettable memory.</h2><br>            
     </div>
 
