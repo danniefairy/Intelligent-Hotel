04da103ee8a5f132774b0a3f360928f6c25a876a
Dannie <dannienctu@gmail.com>
2017-02-23 23:15:05 +0800

    fb test

--- transfer.php
+++ transfer.php
@@ -1,7 +1,12 @@
 <?php
+    include 'db_connect.php';
 	if(isset($_GET['name'])){
 		echo $_GET['name'].$_GET['gender'].$_GET['email'];
-		echo 'yes';
+		$name=$_GET['name'];
+		$gender=$_GET['gender'];
+		$email=$_GET['email'];
+		$insert="INSERT INTO `hotel` (`name`,`gender`,'email') VALUES (\"$name\",\"$gender\",\"email\")";
+		mysqli_query($connect,$insert);
+		echo "send";
 	}
-	echo 'no';
 ?>
