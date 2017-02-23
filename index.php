442602c36f6f3e72e95ed15d82d29421ffa8dd9f
Dannie <dannienctu@gmail.com>
2017-02-23 18:08:25 +0800

    fb test

--- index.php
+++ index.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-  
-  
-    <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Open+Sans:600'>
-
-    <link rel="stylesheet" href="css/style.css">
-
-    <style type="text/css">
-        .sign-up-htm{
-            text-align: center;
-            margin-top:60%;
-        }
-        .login_title{
-            font-size:2vw;
-        }
-    </style>
-
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="./login/login.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./login/login.js"></script>
 </head>
-
 <body>
-  <div class="login-wrap">
-    <div class="login-html">
-        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Introduction</label>
-        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Login</label>
-        <div class="login-form">
-            <div class="sign-in-htm">
-               預留空間
-            </div>
-            <div class="sign-up-htm">
-                <h1 class="login_title">Let's enjoy the journey</h1>
-                <script type="text/javascript" src="./fbapp/fb.js"></script>
-                <div class="fb-login-button" data-scope="public_profile,email" onlogin="checkLoginState();" data-size="xlarge"></div>
-            </div>
+<div class="materialContainer">
+   <div class="box">
+
+        <div class="title">Welcome</div>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
+            保留空間<br>
         </div>
-    </div>
+
+   <div class="overbox">
+      <div class="material-button alt-2"><span class="shape"></span></div>
+
+      <div class="title"  style="text-align:center;">
+        <!--FB登入-->
+        <script type="text/javascript" src="./fbapp/fb.js"></script>
+        <div class="fb-login-button" data-scope="public_profile,email" onlogin="checkLoginState();" data-size="xlarge"></div>
+      </div>
+   </div>
+
 </div>
 </body>
 </html>
