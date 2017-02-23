4ac67c73f2632c6e1b3c53448c829ab55e84fd77
Dannie <dannienctu@gmail.com>
2017-02-23 14:14:54 +0800

    fb test

--- fb_login.php
+++ fb_login.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 <script type="text/javascript" src="./fbapp/fb.js"></script>
-<div class="fb-login-button"></div>
+<div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="false"></div>
 </body>
 </html>
