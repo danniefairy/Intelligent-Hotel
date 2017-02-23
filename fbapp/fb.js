16f0f90cd054ffd9df5b8fcfbe32650634574745
Dannie <dannienctu@gmail.com>
2017-02-23 14:05:48 +0800

    fb test

--- fbapp/fb.js
+++ fbapp/fb.js
@@ -24,7 +24,7 @@ function checkLoginState() {
 
 window.fbAsyncInit = function() {
 	FB.init({
-	appId      : '1529457874011795',
+	appId      : '339043493157430',
 	cookie     : true,  // enable cookies to allow the server to access 
 	                    // the session
 	xfbml      : true,  // parse social plugins on this page
