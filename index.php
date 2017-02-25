43ae10779ee89f106e77b08503c1af356dfbe6c4
Dannie <dannienctu@gmail.com>
2017-02-25 08:43:46 +0800

    fb login test

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <script type="text/javascript" src="./login/login.js"></script>
 </head>
 <body>
-021315165
 <div class="materialContainer">
    <div class="box">
         <div class="title">Intelligent Hotel Service</div>
@@ -24,7 +23,7 @@
         <h2 style="font-size:30px ;">Let's login and fulfill your dream!</h2>
         <!--FB登入-->
         <script type="text/javascript" src="./fbapp/fb.js"></script>
-        <fb:login-button data-scope="public_profile,email" onlogin="checkLoginState();" data-size="xlarge" size="xlarge" >Login with Facebook</fb:login-button>
+        <fb:login-button data-scope="public_profile,email" onlogin="checkLoginState();" data-size="xlarge" size="xlarge" >Login Facebook</fb:login-button>
       </div>
    </div>
 
