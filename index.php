8a49a712846e6d86ff29dc9f3ccf7698dc404574
Dannie <dannienctu@gmail.com>
2017-02-26 18:58:49 +0800

    eamil system connected

--- index.php
+++ index.php
@@ -1,32 +1,242 @@
+<?php
+	if(isset($_GET['send_email']))
+	{
+		echo "<script type=\"text/javascript\">
+			alert(\"Sending sucessfully!\")
+			</script>";
+	}
+?>
+
 <!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="./login/login.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script type="text/javascript" src="./login/login.js"></script>
-</head>
-<body>
-<div class="materialContainer">
-   <div class="box">
-        <div class="title">Intelligent Hotel Service</div>
-            <h2>&nbsp</h2><br>
-            <img style="float:right; height:300px;" src="./login/icon.JPG" >
-            <h2 style="font-size:1.5vw;">I.H.S provide every traveler the most convinent and diversified vacation. Without any annoying preparation for your itinerary, I.H.S can help you schedule your tour and give you a unforgettable memory.</h2><br>            
-    </div>
+<html lang="en">
+	<head>
+		<title>Lobby</title>
+		<meta charset="utf-8">
+		<meta name="format-detection" content="telephone=no" />
+		<link rel="icon" href="images/favicon.ico">
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="stylesheet" href="booking/css/booking.css">
+		<link rel="stylesheet" href="css/camera.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/mycss.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/jquery-migrate-1.2.1.js"></script>
+		<script src="js/script.js"></script>
+		<script src="js/superfish.js"></script>
+		<script src="js/jquery.ui.totop.js"></script>
+		<script src="js/jquery.equalheights.js"></script>
+		<script src="js/jquery.mobilemenu.js"></script>
+		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="js/owl.carousel.js"></script>
+		<script src="js/camera.js"></script>
+		<!--[if (gt IE 9)|!(IE)]><!-->
+		<script src="js/jquery.mobile.customized.min.js"></script>
+		<!--<![endif]-->
+		<script src="booking/js/booking.js"></script>
 
-   <div class="overbox" style="top-margin:150%;">
-      <div class="material-button alt-2"><span class="shape"></span></div>
-
-      <div class="title"  style="text-align:center;">
-        <h2 style="font-size:47px;padding-top:130px; ">Ready for your journey</h2>
-        <h2 style="font-size:30px ;">Let's login and fulfill your dream!</h2>
-        <!--FB登入-->
-        <script type="text/javascript" src="./fbapp/fb.js"></script>
-        <fb:login-button data-scope="public_profile,email" onlogin="checkLoginState();" data-size="xlarge" size="xlarge" >Login with Facebook</fb:login-button>
-      </div>
-   </div>
-
-</div>
-</body>
+		<script>
+			$(document).ready(function(){
+			jQuery('#camera_wrap').camera({
+				loader: false,
+				pagination: false ,
+				minHeight: '444',
+				thumbnails: false,
+				height: '48.375%',
+				caption: true,
+				navigation: true,
+				fx: 'mosaic'
+			});
+			/*carousel*/
+			var owl=$("#owl");
+				owl.owlCarousel({
+				items : 2, //10 items above 1000px browser width
+				itemsDesktop : [995,2], //5 items between 1000px and 901px
+				itemsDesktopSmall : [767, 2], // betweem 900px and 601px
+				itemsTablet: [700, 2], //2 items between 600 and 0
+				itemsMobile : [479, 1], // itemsMobile disabled - inherit from itemsTablet option
+				navigation : true,
+				pagination : false
+				});
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			});
+		</script>
+		<!--[if lt IE 8]>
+		<div style=' clear: both; text-align:center; position: relative;'>
+			<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+				<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+			</a>
+		</div>
+		<![endif]-->
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+		<link rel="stylesheet" media="screen" href="css/ie.css">
+		<![endif]-->
+	</head>
+	<body class="page1" id="top">
+<!--==============================header=================================-->
+		<header>
+			<div class="container_12">
+				<div class="grid_12">
+					<div class="menu_block">
+						<nav class="horizontal-nav full-width horizontalNav-notprocessed">
+							<ul class="sf-menu">
+								<li class="current"><a href="index.php">Home</a></li>
+								<li><a href="index-1.html">HOT TOURS</a></li>
+								<li><a href="index-2.html">SPECIAL OFFERS</a></li>
+								<li><a href="index-3.html">BLOG</a></li>
+								<li><a href="index-4.html">CONTACTS</a></li>
+							</ul>
+						</nav>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<div class="grid_12">
+					<h1>
+						<a href="index.html">
+							<img src="images/logo.png" alt="Your Happy Family">
+						</a>
+					</h1>
+				</div>
+			</div>
+		</header>
+		<div class="slider_wrapper">
+			<div id="camera_wrap" class="">
+				<div data-src="images/slide.jpg">
+					<div class="caption fadeIn">
+						<h2>I.H.S</h2>
+						<div class="price">
+							
+							<span></span>
+						</div>
+						<a href="./about/about.php">About us</a>
+					</div>
+				</div>
+				<div data-src="images/slide1.jpg">
+					<div class="caption fadeIn">
+						<h2>B.N.B</h2>
+						<div class="price">
+							
+							<span></span>
+						</div>
+						<a href="#">LEARN MORE</a>
+					</div>
+				</div>
+				<div data-src="images/slide2.jpg">
+					<div class="caption fadeIn">
+						<h2>Delicacy</h2>
+						<div class="price">
+							
+							<span></span>
+						</div>
+						<a href="#">LEARN MORE</a>
+					</div>
+				</div>
+				<!--add here-->
+				<div data-src="images/slide3.jpg">
+					<div class="caption fadeIn">
+						<p class="arrange">Arrangement</p>
+						<div class="price">
+							
+							<span></span>
+						</div>
+						<a href="#">LEARN MORE</a>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--==============================Content=================================-->
+		<div class="content"><div class="ic">More Website Templates @ TemplateMonster.com - February 10, 2014!</div>
+			<div class="container_12">
+				<div class="grid_4">
+					<div class="banner">
+						<img src="images/ban_img1.jpg" alt="">
+						<div class="label">
+							<div class="title">Souvenir<br><br></div>
+							<div class="price"><span></span></div>
+							<a href="#">LEARN MORE</a>
+						</div>
+					</div>
+				</div>
+				<div class="grid_4">
+					<div class="banner">
+						<img src="images/ban_img2.jpg" alt="">
+						<div class="label">
+							<div class="title">Traffic Information</div>
+							<div class="price"><span></span></div>
+							<a href="#">LEARN MORE</a>
+						</div>
+					</div>
+				</div>
+				<div class="grid_4">
+					<div class="banner">
+						<img src="images/ban_img3.jpg" alt="">
+						<div class="label">
+							<div class="title">Mobile Payment<br><br></div>
+							<div class="price"><span></span></div>
+							<a href="#">LEARN MORE</a>
+						</div>
+					</div>
+				</div>
+				<div class="clear"></div>
+				<!--safe place connect db-->
+				<div class="grid_12">
+					<h3 class="head1">Latest News</h3>
+				</div>
+				<div class="grid_4">
+					<div class="block1">
+						<time datetime="2014-01-01">10<span>Jan</span></time>
+						<div class="extra_wrapper">
+							<div class="text1 col1"><a href="#">Aliquam nibh</a></div>
+							Proin pharetra luctus diam, any scelerisque eros convallisumsan. Maecenas vehicula egestas
+						</div>
+					</div>
+				</div>
+				<div class="grid_4">
+					<div class="block1">
+						<time datetime="2014-01-01">21<span>Jan</span></time>
+						<div class="extra_wrapper">
+							<div class="text1 col1"><a href="#">Etiam dui eros</a></div>
+							Any scelerisque eros vallisumsan. Maecenas vehicula egestas natis. Duis massa elit, auctor non
+						</div>
+					</div>
+				</div>
+				<div class="grid_4">
+					<div class="block1">
+						<time datetime="2014-01-01">15<span>Feb</span></time>
+						<div class="extra_wrapper">
+							<div class="text1 col1"><a href="#">uamnibh Edeto</a></div>
+							Ros convallisumsan. Maecenas vehicula egestas venenatis. Duis massa elit, auctor non
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--==============================footer=================================-->
+		<footer>
+			<div class="container_12">
+				<div class="grid_12">
+					<div class="socials">
+						<a href="#" class="fa fa-facebook"></a>
+						<a href="#" class="fa fa-twitter"></a>
+						<a href="#" class="fa fa-google-plus"></a>
+					</div>
+					<div class="copy">
+						Intelligent Hotel System | by Dannie
+					</div>
+				</div>
+			</div>
+		</footer>
+		<script>
+			$(function (){
+				$('#bookingForm').bookingForm({
+					ownerEmail: '#'
+				});
+			})
+			$(function() {
+				$('#bookingForm input, #bookingForm textarea').placeholder();
+			});
+		</script>
+	</body>
 </html>
+
