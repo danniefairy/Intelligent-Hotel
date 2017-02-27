b0aee3448e570038757758c32cf354d97a9b08ab
Dannie <dannienctu@gmail.com>
2017-02-27 23:41:56 +0800

    book room

--- Home/book.php
+++ Home/book.php
@@ -307,7 +307,7 @@
 							</select>
 							<div class="clear"></div>
 							<em>Payment</em>
-							<select id="payment" name="Rooms" class="tmSelect auto"  data-constraints="">
+							<select id="payment" name="Rooms"  data-class="tmSelect tmSelect2" data-constraints="">
 								<option>&nbsp &nbsp &nbsp &nbsp</option>
 								<option id="credit_card" onclick="credit()">Credit Card</option>
 								<option id="paypal" onclick="paypal()">Paypal</option>
