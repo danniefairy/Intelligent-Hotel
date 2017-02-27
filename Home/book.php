afa3441957157b2bdb078b04926427165f4d1118
Dannie <dannienctu@gmail.com>
2017-02-27 20:26:41 +0800

    book room

--- Home/book.php
+++ Home/book.php
@@ -340,8 +340,10 @@
 							document.getElementById(comfort).checked=true;
 						</script>
 
-						<br>
+
 						<div class="clear"></div>
+						<br>
+						<br>
 						<div class="tmTextarea">
 							<textarea id="message" name="Message" placeHolder="Message" data-constraints='@NotEmpty @Required @Length(min=20,max=999999)'></textarea>
 						</div>
