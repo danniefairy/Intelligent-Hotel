106974c99b1b4a9e07e07f4ff868a33e574eec5a
Dannie <dannienctu@gmail.com>
2017-02-27 15:19:51 +0800

    book room

---
+++ Home/bnb/book_info.php
@@ -0,0 +1,14 @@
+<?php
+	$name=$_GET['name'];
+	$email=$_GET['email'];
+	$country=$_GET['country'];
+	$phone=$_GET['phone'];
+	$check_in=$_GET['check_in'];
+	$check_out=$_GET['check_out'];
+	$button=$_GET['button'];
+	$person=$_GET['person'];
+	$room=$_GET['room'];
+	$message=$_GET['message'];
+
+	echo $name."<br>".$email."<br>".$country."<br>".$phone."<br>".$check_in."<br>".$check_out."<br>".$button."<br>".$person."<br>".$room."<br>".$message."<br>";
+?>
