ff71bf195b210d06fa7c30d90eb1275b621962be
Dannie <dannienctu@gmail.com>
2017-02-27 11:46:52 +0800

    session credit card

--- Home/index.php
+++ Home/index.php
@@ -28,12 +28,12 @@
 		$id=$_SESSION['fb_id'];
 		$sql="SELECT `card_name` FROM hotel Where `id`=\"$id\"";
 		$result=mysqli_query($connect,$sql);
-		/*if(mysqli_fetch_array($result)!=""){
+		if(mysqli_fetch_array($result)!=""){
 			$_SESSION['card_bind']=1;
 			echo "<script type=\"text/javascript\">
 			alert(\"Binding credit!\")
 			</script>";
-		}*/
+		}
 	}
 ?>
 
