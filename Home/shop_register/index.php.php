a1cf1b674cba1a5c621169476f07fd40149f7f0f
Dannie <dannienctu@gmail.com>
2017-02-28 20:35:30 +0800

    book room

---
+++ Home/shop_register/index.php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="shop_register.css">
+</head>
+<body>
+<div class="login">
+    <h1>Shop Register</h1>
+    <form method="post">
+        <input type="text" name="u" placeholder="Company Name" required="required" />
+        <input type="number" name="p" placeholder="Employer Identification Number" required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">Register</button>
+    </form>
+</div>
+
+</body>
+</html>
