fbae1b32519748f00eeb1fc14405828d865c4414
Dannie <dannienctu@gmail.com>
2017-02-28 21:18:11 +0800

    shop register error

--- Home/shop_register/register.php
+++ Home/shop_register/register.php
@@ -9,10 +9,21 @@
 		$long=$_POST['long'];
 		
 		include 'connect_db.php';
-		$insert="INSERT INTO `shop` (`db_id`,`company_name`,`ein`,`latitude`,`longitude`) VALUES (\"$db_id\",\"$name\",\"$ein\",\"$lat\",\"$long\")";
-		mysqli_query($connect,$insert);
 
-		echo $db_id."<br>".$name."<br>".$ein."<br>".$lat."<br>".$long."<br>".$insert;
+		$search="SELECT * FROM shop WHERE `db_id`=$db_id";
+		$result=mysqli_query($connect,$search);
+		if(mysqli_fetch_array($result))
+		{
+			echo "yes";
+		}
+		else{
+			$insert="INSERT INTO `shop` (`db_id`,`company_name`,`ein`,`latitude`,`longitude`) VALUES (\"$db_id\",\"$name\",\"$ein\",\"$lat\",\"$long\")";
+			mysqli_query($connect,$insert);
+		}
+
 		
+		
+
+		//echo $db_id."<br>".$name."<br>".$ein."<br>".$lat."<br>".$long."<br>".$insert;	
 	}
 ?>
