b19682f66eaa4fffab7c5e1518c2416e8505729a
Dannie <dannienctu@gmail.com>
2017-02-28 21:08:58 +0800

    shop register error

--- Home/shop_register/register.php
+++ Home/shop_register/register.php
@@ -10,9 +10,9 @@
 		
 
 		$insert="INSERT INTO `shop` (`db_id`,`company_name`,`ein`,`latitude`,`longitude`) VALUES (\"$db_id\",\"$name\",\"$ein\",\"$lat\",\"$long\")";
-		mysqli_query($connect,$insert);
+		echo mysqli_query($connect,$insert)->error;
 
 		echo $db_id."<br>".$name."<br>".$ein."<br>".$lat."<br>".$long.$insert;
-		echo "<br>".mysqli_error($connect);
+		
 	}
 ?>
