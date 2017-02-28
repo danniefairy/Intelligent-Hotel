6466eedac337457317f75190767aa3eae2f9c460
Dannie <dannienctu@gmail.com>
2017-02-28 20:38:57 +0800

    shop register system

--- Home/shop_register/index.php
+++ Home/shop_register/index.php
@@ -9,7 +9,7 @@
     <h1>Shop Register</h1>
     <form method="post">
         <input type="text" name="u" placeholder="Company Name" required="required" />
-        <input type="number" name="p" placeholder="Employer Identification Number" required="required" />
+        <input type="text" name="p" placeholder="Employer Identification Number" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">Register</button>
     </form>
 </div>
