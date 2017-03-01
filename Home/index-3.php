cc0b0ac4472115cda7061c46ac194b30b68a5878
Dannie <dannienctu@gmail.com>
2017-03-01 22:37:55 +0800

    store upload index-3

--- Home/index-3.php
+++ Home/index-3.php
@@ -81,7 +81,56 @@
 			<!--左邊-->
 				<div class="grid_7">
 					<h3>Manage Commodity</h3>
+					<form action="upload_get.php" method="post" enctype="multipart/form-data">
+							Commodity:<br/>
+							<input type="text" name="file_name" placeholder="Name"><br/><br/>
+							Picture:<br/>
+							<input type="file" name="file"><br/><br/>
+							Description:<br>
+							<textarea name="description" placeholder="Description of your commodity!"></textarea><br><br>
+							<input type="submit" value="送出">	
+							<br><br>
+							<hr>	
+						</form>
 
+						<?php
+						//利用GET刪除檔案
+						if(isset($_GET['delete']))
+							unlink("./upload/".$_GET['delete']);
+
+						//利用GET重新命名
+						if(isset($_GET['new']))
+							rename($_GET['old'], $_GET['new']);
+						
+
+						/*
+						*讀取資料夾目錄
+						*/
+						$dirlist=scandir('./upload/');
+						$i=count($dirlist);
+
+						echo "<table>";
+						//因為前兩個為.、..所以真正檔名從矩陣2開始
+						for($k=2;$k<$i;$k++){
+							$name=$dirlist[$k];
+							echo "<tr>";
+								echo "<td>";
+									echo "<a href=./upload/$name>$name</a>";
+								echo "</td>";
+							//使用get來改變id
+								echo "<td>";
+									echo "&nbsp&nbsp<a href=\"?delete=$name\">delete</a>";
+								echo "</td>";
+							//更新名稱
+								echo "<td>";
+									echo "&nbsp&nbsp<a href=\"upload_rename.php?rename=./upload/$name\">rename</a>";
+								echo "</td>";
+							echo "</tr>";
+						
+						}
+						echo "</table>";
+
+						?>
 				</div>
 			<!--左邊-->
 
---
+++ Home/upload_get.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload_file_get</title>
+</head>
+<body>
+	<?php
+		/*
+		*$_FILES['UpFile']['tmp_name']上傳檔案存在server端地站存檔路徑以及檔名
+		*$_FILES['UpFile']['name']上傳檔案在用戶端的原始檔名
+		*$_FILES['UpFile']['type']上傳檔案類型
+		*$_FILES['UpFile']['size']上傳檔案大小
+		*$_FILES['UpFile']['error']錯誤代碼 (如果錯誤代碼為UPLOAD_ERR_OK表示上傳成功)
+		*/
+
+		//要傳到目前位置所建新的資料夾要用"./資料夾名稱/""
+		$upload_dir='./upload/';
+		if($_FILES['file']['error']==UPLOAD_ERR_OK){
+			move_uploaded_file($_FILES['file']['tmp_name'], $upload_dir.$_FILES['file']['name']);
+			echo '上傳成功<br>';
+			echo "原始檔名: ".$_FILES['file']['name']." <br>";
+			echo "檔案類型: ".$_FILES['file']['type']." <br>";
+			echo "檔案大小: ".$_FILES['file']['size']." <br>";
+			echo "暫存檔名: ".$_FILES['file']['tmp_name']." <br>";
+			//echo explode(".",$_FILES['file']['name'])[1];
+			rename("./upload/".$_FILES['file']['name'], "./upload/".$_POST['file_name'].".".explode(".",$_FILES['file']['name'])[1]);
+
+			
+		}
+		else{
+			echo "上傳失敗";
+		}
+
+	?>
+	<form>
+		<input type ="button" onclick="location.href='upload.php'" value="回到上一頁"></input>
+	</form>
+	
+</body>
+</html>
+
+	
