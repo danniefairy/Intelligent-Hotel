d55881e56c9e27fbfb3c77c2b1d18d7e196af4a7
Dannie <dannienctu@gmail.com>
2017-03-01 23:44:52 +0800

    store to db

--- Home/upload_rename.php
+++ Home/upload_rename.php
@@ -4,17 +4,17 @@
 	<title>Rename</title>
 </head>
 <body>
-	<form name="rename" method="get">
+	<form method="get">
 		舊檔案名稱:
 		<input type="text" name="old" maxlength="50" value="<?php echo $_GET['rename'];?>">
 		請輸入新檔案名稱:
 		<input id="new_name" type="text" name="new" maxlength="50" >
-		<input type="submit" value="修改" onclick="rename()">
+		<button  onclick="rename()"></button>
 	</form>
 <script type="text/javascript">
 	function rename(){
 		var name = document.getElementById("new_name").value;
-		alert("./index-3.php?manager=1&new="+name);
+		//alert("./index-3.php?manager=1&new="+name);
 		window.location.replace("./index-3.php?manager=1&new="+name);
 	}
 </script>
