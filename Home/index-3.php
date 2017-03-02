261110d34a44a1e0daf79934539203f98c7faa35
Dannie <dannienctu@gmail.com>
2017-03-02 22:06:55 +0800

    store to db

--- Home/index-3.php
+++ Home/index-3.php
@@ -52,7 +52,7 @@
 				    background-color: #dddddd;
 				}
 
-				input[type=text,number], select {
+				input[type=text], select {
 			    width: 100%;
 			    padding: 12px 20px;
 			    margin: 8px 0;
@@ -61,6 +61,17 @@
 			    border-radius: 4px;
 			    box-sizing: border-box;
 				}
+
+				input[type=number], select {
+			    width: 100%;
+			    padding: 12px 20px;
+			    margin: 8px 0;
+			    display: inline-block;
+			    border: 1px solid #ccc;
+			    border-radius: 4px;
+			    box-sizing: border-box;
+				}
+
 				input[type=submit] {
 				    width: 100%;
 				    background-color: #4CAF50;
