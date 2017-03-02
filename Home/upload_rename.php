e91a18a65eaa18fda08ec7095dc2f64d31192fa4
Dannie <dannienctu@gmail.com>
2017-03-02 22:00:39 +0800

    store to db

--- Home/upload_rename.php
+++ Home/upload_rename.php
@@ -3,6 +3,27 @@
 <head>
 	<title>Rename</title>
 </head>
+<style type="text/css">
+	input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=button] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+</style>
 <body>
 <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);">
 	<form >
