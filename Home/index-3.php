74a28713395e96fe2d91f7b2148b8830916d0f65
Dannie <dannienctu@gmail.com>
2017-03-02 21:41:43 +0800

    store to db

--- Home/index-3.php
+++ Home/index-3.php
@@ -117,12 +117,12 @@
 									echo "<a href=./shop_register/upload/$name>$name</a>";
 								echo "</td>";
 							//使用get來改變id
-								echo "<td";
-									echo "&nbsp&nbsp&nbsp&nbsp<a href=\"?manager=1&delete=$name\">delete</a>";
+								echo "<td>";
+									echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href=\"?manager=1&delete=$name\">delete</a>";
 								echo "</td>";
 							//更新名稱
 								echo "<td>";
-									echo "&nbsp&nbsp&nbsp&nbsp<a href=\"upload_rename.php?rename=./shop_register/upload/$name\">rename</a>";
+									echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href=\"upload_rename.php?rename=./shop_register/upload/$name\">rename</a>";
 								echo "</td>";
 							echo "</tr>";
 						
