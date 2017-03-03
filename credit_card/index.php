d64fafe1768654edb245cbeb44844508ea6695e8
Dannie <dannienctu@gmail.com>
2017-03-03 17:38:07 +0800

    credit card style

--- credit_card/index.php
+++ credit_card/index.php
@@ -20,23 +20,23 @@
 ?>
 
 <!--credit card-->
-<div class="form" style=""">
-  <h1 style="text-align:center;">Credit Card</h1>
+<div class="form"">
+  <h1 style="text-align:center; font-size:40px;">Credit Card</h1>
   <hr>
 
   <form id="form" action="get.php">
 
 
-      <h2>Name on card</h2>
+      <h1>Name on card</h1>
       <input type="text" name="Name" id="name" value=""></input>
 
-      <h2>Card number</h2>
+      <h1>Card number</h1>
       <input type="number" name="Number" id="number" value=""></input>
       
-      <h2>CVV</h2>
+      <h1>CVV</h1>
       <input type="text" name="CVV" id="cvv" value=""></input>
       
-      <h2>Expires</h2>
+      <h1>Expires</h1>
         <label for="month" >Month</label>
         <br>
           <select id='month' name="month">
@@ -69,7 +69,7 @@
           <option value='2021'>2021</option>
           </select>
 
-      <br>
+      <br><br>
       <input type='submit' value="Submit"></input>
   </form>
 
