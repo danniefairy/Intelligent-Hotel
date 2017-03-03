5a119f395c4d2baa25164c5fc1e3876ec0ac2f9b
Dannie <dannienctu@gmail.com>
2017-03-03 17:13:47 +0800

    credit card stytle

--- Home/index-1.php
+++ Home/index-1.php
@@ -405,7 +405,7 @@
 								var country=document.getElementById('country').value;
 								var check_in=document.getElementById('in').value;
 								var check_out=document.getElementById('out').value;
-								var button=$('input[id="Radio"]:checked').val();;
+								var button=$('input[id="Radio"]:checked').val();
 								var adult=document.getElementById('adult').value;
 								var child=document.getElementById('child').value;
 								var room=document.getElementById('room').value;
