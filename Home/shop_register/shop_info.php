7232290dbf5ab1ffd61de6048a82081307060a9f
Dannie <dannienctu@gmail.com>
2017-03-05 22:22:30 +0800

    shop info

--- Home/shop_register/shop_info.php
+++ Home/shop_register/shop_info.php
@@ -163,7 +163,7 @@
 
 	  	while($row=mysqli_fetch_array($result)){
 	  	echo "{";
-	    echo "name: \"$row[2]\"".",";
+	    echo "name: \"explode(".", $row[2])[0]\"".",";
 	    echo "id: \"$row[0]\"";
 	  	echo "},";
   		}
