be251b42063f4590856cc14caa6d1b8d907a8d88
Dannie <dannienctu@gmail.com>
2017-03-07 11:18:03 +0800

    current location at register index

--- Home/shop_register/index.php
+++ Home/shop_register/index.php
@@ -17,6 +17,35 @@
 <head>
     <title></title>
     <link rel="stylesheet" type="text/css" href="shop_register.css">
+
+            <!--google map-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6h46-lqQIkEoHqwEiV1ub4vmhe8s92Ws&callback=initMap"
+        async defer></script>
+    <script>
+
+    //-------current location-------
+
+    if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	    var pos = {
+	    	lat: position.coords.latitude,
+	        lng: position.coords.longitude
+	    };
+
+	  	document.getElementById('lat').value=pos['lat'];
+	  	document.getElementById('lng').value=pos['lng'];
+	    }, function() {
+	      //handleLocationError(true, infoWindow, map.getCenter());
+	    });
+	    //alert(pos);
+
+	} 
+
+  //-------current location-------
+
+
+    </script>
+    <!--google map-->
 </head>
 <body>
 <div class="login">
@@ -24,8 +53,8 @@
     <form method="post" action="register.php">
         <input type="text" name="name" placeholder="Company Name" required="required" />
         <input type="text" name="ein" placeholder="Employer Identification Number" required="required" />
-        <input type="text" name="lat" placeholder="Latitude" required="required" />
-        <input type="text" name="long" placeholder="Longitude" required="required" />
+        <input id="lat" type="text" name="lat" placeholder="Latitude" required="required" />
+        <input id="lng" type="text" name="long" placeholder="Longitude" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">Register</button>
     </form>
 </div>
