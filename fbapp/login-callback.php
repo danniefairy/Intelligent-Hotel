39843bf28fe26dbc1cfd11190a9f947a78bd4a29
Dannie <dannienctu@gmail.com>
2017-03-08 19:35:44 +0800

    fb friend list

--- fbapp/login-callback.php
+++ fbapp/login-callback.php
@@ -27,7 +27,7 @@ if (isset($accessToken)) {
 
   try {
   
-    $requestProfile = $fb->get("/me?fields=name,id,gender,email,friendlists");
+    $requestProfile = $fb->get("/me?fields=name,id,gender,email,friends");
     $profile = $requestProfile->getGraphNode()->asArray();
   } catch(Facebook\Exceptions\FacebookResponseException $e) {
     // When Graph returns an error
@@ -37,7 +37,7 @@ if (isset($accessToken)) {
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
   }
   //endpoints 跟 permission request 用的不一樣
-  echo count($profile['friendlists']);
+  echo count($profile['friends']);
   $_SESSION['fb_id']=$profile['id'];
   $_SESSION['name']=$profile['name'];
   //header('location: ../transfer.php?name='.$profile['name']."&fb_id=".$profile['id']."&gender=".$profile['gender']."&email=".$profile['email']);
