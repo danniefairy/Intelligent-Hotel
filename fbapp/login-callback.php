91f0d95b79326348f6e29d56c534ccb33965bd15
Dannie <dannienctu@gmail.com>
2017-03-08 19:15:43 +0800

    fb friend list

--- fbapp/login-callback.php
+++ fbapp/login-callback.php
@@ -37,7 +37,7 @@ if (isset($accessToken)) {
     echo 'Facebook SDK returned an error: ' . $e->getMessage();
   }
   
-  echo $profile['friends'];
+  echo $profile['friends'][0];
   $_SESSION['fb_id']=$profile['id'];
   $_SESSION['name']=$profile['name'];
   //header('location: ../transfer.php?name='.$profile['name']."&fb_id=".$profile['id']."&gender=".$profile['gender']."&email=".$profile['email']);
